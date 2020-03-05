03508f58faeec936e4766cd77498e14787c0b69f
tuanjing <tuanjing@outlook.com>
2020-03-05 10:57:32 +0800

    创建 button-group.vue

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('ym-button', Button)
 Vue.component('ym-icon', Icon)
+Vue.component('ym-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="ym-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "YmButtonGroup"
+}
+</script>
+
+<style lang="scss" scoped>
+.ym-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+
+  > .ym-button {
+    border-radius: 0;
+    margin-left: -1px;
+
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+
+    &:hover {
+      z-index: 1;
+    }
+  }
+}
+</style>
