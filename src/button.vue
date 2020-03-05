79785d5d05e708b5b394a2865c6214a8f8656988
tuanjing <tuanjing@outlook.com>
2020-03-05 09:51:46 +0800

    button.vue 添加 icon 和 iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,18 @@
 <template>
-  <button class="ym-button">hi yumi</button>
+  <button class="ym-button" :class="{ [`ym-icon-${iconPosition}`]: true }">
+    <svg v-if="icon" class="ym-icon">
+      <use :xlink:href=`#icon-${icon}`></use>
+    </svg>
+    <div class="ym-button-content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
 export default {
-  name: "YmButton"
+  name: "YmButton",
+  props: ['icon', 'iconPosition']
 }
 </script>
 
@@ -16,6 +24,10 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
 
   &:hover {
     border-color: var(--border-color-hover);
@@ -27,6 +39,27 @@ export default {
 
   &:focus {
     outline: none;
+  }
+
+  > .ym-icon {
+    order: 1;
+    margin-right: .1em;
+  }
+
+  > .ym-button-content {
+    order: 2;
+  }
+
+  &.ym-icon-right {
+    > .ym-button-content {
+      order: 1;
+    }
+
+    > .ym-icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: .1em;
+    }
   }
 }
 </style>
