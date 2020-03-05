8fbe034ee08c44db0024b72b19b6b144d39f7655
tuanjing <tuanjing@outlook.com>
2020-03-05 11:32:18 +0800

    使用 chai.expect 添加对属性的测试用例

--- src/app.js
+++ src/app.js
@@ -20,3 +20,93 @@ new Vue({
     }
   }
 })
+
+// 单元测试
+import chai from 'chai'
+const expect = chai.expect
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  })
+  // vm.$mount('#test')
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  expect(useElement.getAttribute('xlink:href')).to.eq('#icon-settings')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  // vm.$mount('#test')
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  expect(useElement.getAttribute('xlink:href')).to.eq('#icon-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  })
+
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  vm.$mount(div)
+  let svgElement = vm.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svgElement)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  vm.$mount(div)
+  let svgElement = vm.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svgElement)
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount()
+  vm.$on('click', function () {
+    // 期望函数被执行
+  })
+  let button = vm.$el
+  button.click()
+  vm.$el.remove()
+  vm.$destroy()
+}
