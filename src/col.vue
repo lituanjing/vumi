2d1599fa0eaa8afe5d68c1595326803013e2ef92
tuanjing <tuanjing@outlook.com>
2020-03-10 12:36:58 +0800

    基本实现 row 和 col 的功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="ym-col" :class="[`ym-col-${span}`]">
-
+  <div
+    :class="[span && `ym-col-${span}`, offset && `ym-col-offset-${offset}`]"
+    :style="{ paddingLeft: gutter / 2 + 'px', paddingRight: gutter / 2 + 'px' }"
+    class="ym-col">
+    <slot></slot>
   </div>
 </template>
 
@@ -8,7 +11,17 @@
 export default {
   name: "YmCol",
   props: {
-    span: [Number, String]
+    span: {
+      type: [Number, String],
+    },
+    offset: {
+      type: [Number, String],
+    },
+  },
+  data () {
+    return {
+      gutter: 0
+    }
   }
 }
 </script>
@@ -16,15 +29,20 @@ export default {
 <style lang="scss" scoped>
 .ym-col {
   height: 60px;
-  background-color: lightblue;
   border: 1px solid gray;
   width: 50%;
 
-  $class: ym-col-;
-
+  $class-prefix: ym-col-;
   @for $n from 1 through 24 {
-    &.#{$class}#{$n} {
+    &.#{$class-prefix}#{$n} {
       width: ($n / 24) * 100%;
+    }
+  }
+
+  $class-prefix: ym-col-offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n / 24) * 100%;
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="ym-row">
+  <div
+    :style="{ marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px' }"
+    class="ym-row">
     <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
-  name: "YmRow"
+  name: "YmRow",
+  props: {
+    gutter: {
+      type: [Number, String]
+    }
+  },
+  mounted () {
+    this.$children.forEach(vm => {
+      vm.gutter = this.gutter
+    })
+  }
 }
 </script>
 
