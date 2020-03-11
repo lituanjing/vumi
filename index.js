849fb36ef09817eee88d35431fec34d59f9f0a99
tuanjing <tuanjing@outlook.com>
2020-03-11 12:44:35 +0800

    导出 input, row, col ， 更新版本

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import YmButton from './src/button'
 import YmButtonGroup from './src/button-group'
 import YmIcon from './src/icon'
+import YmInput from './src/input'
+import YmRow from './src/row'
+import YmCol from './src/col'
 
 export {
   YmButton,
   YmButtonGroup,
   YmIcon,
+  YmInput,
+  YmRow,
+  YmCol,
 }
