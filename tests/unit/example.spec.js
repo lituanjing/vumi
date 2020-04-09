86cd3f7802b1afecf6b5796cdb9910d0520bf019
tuanjing <tuanjing@outlook.com>
2020-04-09 11:01:17 +0800

    更新单元测试

--- tests/unit/example.spec.js
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
-import YmButton from '@/button.vue'
-
-describe('YmButton', () => {
-  it('is ok', () => {
-    expect(1).to.eq(1)
-  })
-})
